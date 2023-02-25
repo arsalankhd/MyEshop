a5a1905829a4ca6375eea2766c922177fb5b7dcb
Arsalan Khodadadi <arsalan.sc30@gmail.com>
2023-02-25 19:41:44 +0330

    some edits in admin panel and user model

--- MyEshop/Models/Users.cs
+++ MyEshop/Models/Users.cs
@@ -10,12 +10,16 @@ namespace MyEshop.Models
         public int UserId { get; set; }
         [Required]
         [MaxLength(300)]
+        [Display(Name = "ایمیل")]
         public string Email { get; set; }
         [Required]
         [MaxLength(50)]
+        [Display(Name = "کلمه عبور")]
         public string Password { get; set; }
         [Required]
+        [Display(Name = "تاریخ ثبت نام")]
         public DateTime RegisterDate { get; set; }
+        [Display(Name = "آیا ادمین است؟")]
         public bool IsAdmin { get; set; }
 
         public List<Order> Orders { get; set; }
